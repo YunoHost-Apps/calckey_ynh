318373c7d864c88c5595e439a1630290c00f1c78
oufmilo <44617467+oufmilo@users.noreply.github.com>
2023-02-27 09:58:28 +0100

    Fix updater.sh

--- .github/workflows/updater.sh
+++ .github/workflows/updater.sh
@@ -17,8 +17,8 @@
 #=================================================
 
 # Fetching information
-current_version=$(cat manifest.json | jq -j '.version|split("~")[0]')
-repo=$(cat manifest.json | jq -j '.upstream.code|split("https://github.com/")[1]')
+current_version=$(cat manifest.tome | jq -j '.version|split("~")[0]')
+repo=$(cat manifest.toml | jq -j '.upstream.code|split("https://github.com/")[1]')
 # Some jq magic is needed, because the latest upstream release is not always the latest version (e.g. security patches for older versions)
 latest_release_json=$(curl --silent "https://codeberg.org/api/v1/repos/calckey/calckey/releases" | jq -r '[.[] | select( .prerelease != true )][0]')
 version=$(echo $latest_release_json | jq -r '.tag_name')
@@ -129,7 +129,7 @@ done
 #=================================================
 
 # Replace new version in manifest
-echo "$(jq -s --indent 4 ".[] | .version = \"$version~ynh1\"" manifest.json)" > manifest.json
+echo "$(jq -s --indent 4 ".[] | .version = \"$version~ynh1\"" manifest.toml)" > manifest.toml
 
 # No need to update the README, yunohost-bot takes care of it
 
