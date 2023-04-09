6fee0f2c48a3cc724b1a3f034e432eb8034543dc
oufmilo <44617467+oufmilo@users.noreply.github.com>
2023-04-09 10:26:40 +0200

    Update updater.sh

--- .github/workflows/updater.sh
+++ .github/workflows/updater.sh
@@ -19,7 +19,7 @@
 
 # Fetching information
 current_version=$(cat manifest.toml | tomlq -j '.version|split("~")[0]')
-repo=$(cat manifest.toml | tomlq -j '.upstream.code|split("https://github.com/")[1]')
+repo=$(cat manifest.toml | tomlq -j '.upstream.code|split("https://codeberg.org/")[1]')
 
 # Some jq magic is needed, because the latest upstream release is not always the latest version (e.g. security patches for older versions)
 latest_release_json=$(curl --silent "https://codeberg.org/api/v1/repos/calckey/calckey/releases" | jq -r '[.[] | select( .prerelease != true )][0]')
